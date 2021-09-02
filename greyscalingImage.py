3819edb45a2a7f98e4f3861f1895dccb7ca92a11
Nasir Khurshid <56188444+nasirkhurshid@users.noreply.github.com>
2021-09-02 16:52:07 +0500

    Add files via upload

--- greyscalingImage.py
+++ greyscalingImage.py
@@ -5,22 +5,35 @@ All right reserved.
 '''
 import pprint as p              #imported pprint for pretty output
 
+def validate(val):              #function for validating values
+    for x in range(len(val)):
+        if val[x] > 255:
+            val[x] = 255
+        elif val[x] < 0:
+            val[x] = 0
+
 def func(pxl):                  #function for greyscaling each pixel
     pl = len(pxl)               #getting size of pixel
     if pl > 0 and pl < 4:       
         if pl == 1:            #greyscaling each pixel by using if conditions 
+            validate(pxl[0])
             entry = int(sum(pxl[0])/3)
             pxl[0] = [entry, entry, entry]
         elif pl == 2:
+            validate(pxl[0])
             entry = int(sum(pxl[0])/3)
             pxl[0] = [entry, entry, entry]
+            validate(pxl[1])
             entry = int(sum(pxl[1])/3)
             pxl[1] = [entry, entry, entry]            
         else:
+            validate(pxl[0])
             entry = int(sum(pxl[0])/3)
             pxl[0] = [entry, entry, entry]
+            validate(pxl[1])
             entry = int(sum(pxl[1])/3)
             pxl[1] = [entry, entry, entry]
+            validate(pxl[2])
             entry = int(sum(pxl[2])/3)
             pxl[2] = [entry, entry, entry]
     else:
@@ -52,9 +65,12 @@ def greyscale(img):         #basic function for greyscaling
 #img = [[[1,2,3], [3,5,1], [8,3,1]],[[1,2,3], [3,5,1], [8,3,1]],[[1,2,3], [3,5,1], [8,3,1]]]
 
 img = [
-  [[0, 0, 0], [0, 0, 157]],
+  [[0, 0, 258], [0, 0, 157]],
   [[1, 100, 0], [0, 10, 0]]
 ]
+
+#img1 = [2,-1, 333]
+#validate(img1)
 
 print("Printing Old Image: ")
 p.pprint(img)
